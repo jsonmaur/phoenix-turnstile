053dfe0dcefd8b9948a021eaaaa27daf4d90d4d3
Jason Maurer <jsonmaur@fastmail.com>
2023-04-24 22:16:24 -0600

    Don't worry about the updated and destroyed hook events

--- priv/static/phoenix_turnstile.js
+++ priv/static/phoenix_turnstile.js
@@ -4,22 +4,14 @@ export const TurnstileHook = {
 
     this.handleEvent("turnstile:refresh", (event) => {
       if (!event.id || event.id === this.el.id) {
-        this.updated()
+        turnstile.reset(this.el)
       }
     })
 
     this.handleEvent("turnstile:remove", (event) => {
       if (!event.id || event.id === this.el.id) {
-        this.destroyed()
+        turnstile.remove(this.el)
       }
     })
-  },
-
-  updated() {
-    turnstile.reset(this.el)
-  },
-
-  destroyed() {
-    turnstile.remove(this.el)
   }
 }
